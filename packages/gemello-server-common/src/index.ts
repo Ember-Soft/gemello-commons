c5cfc6662649725d95b0089903cdf556d9c8cf5f
michalraszczuk <michal.raszczuk@bshg.com>
2023-09-08 18:18:01 +0200

    added integration test hook builder

--- packages/gemello-server-common/src/index.ts
+++ packages/gemello-server-common/src/index.ts
@@ -1,8 +1,10 @@
 export { GemelloUserContext } from "./decorators/gemelloUserContext.decorator";
+export { IntegrationTestHookBuilder } from "./testing/integration-tests/buildIntegrationTestHook";
 export { RepositoryError } from "./errors/repository.error";
 export { UseError } from "./utils/useError";
 export { UseRepositoryError } from "./decorators/useRepositoryError.decorator";
 export { User } from "./decorators/user.decorator";
 
+export type { BuildIntegrationTestHookProps } from "./testing/integration-tests/buildIntegrationTestHook";
 export type { GemelloUser } from "./types/user";
 export type { OmitNulls } from "./types/omitNulls";
---
+++ packages/gemello-server-common/src/testing/integration-tests/buildIntegrationTestHook.ts
@@ -0,0 +1,71 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { PrismaClient } from "@prisma/client";
+import { ModuleMetadata } from "@nestjs/common";
+
+export interface BuildIntegrationTestHookProps<T extends PrismaClient> {
+  PrismaService: { new (): T };
+  clearDb: (prisma: T) => Promise<void>;
+  fillDb: (prisma: T) => Promise<void>;
+  callback: (moduleRef: TestingModule) => Promise<void>;
+  moduleMetadata: ModuleMetadata;
+}
+
+const emptyFn = async () => {};
+
+export class IntegrationTestHookBuilder<T extends PrismaClient> {
+  private buildProps: BuildIntegrationTestHookProps<T> = {
+    PrismaService: PrismaClient,
+    callback: emptyFn,
+    fillDb: emptyFn,
+    clearDb: emptyFn,
+    moduleMetadata: {},
+  };
+
+  public setPrismaClient(Prisma: { new (): T }) {
+    this.buildProps.PrismaService = Prisma;
+    return this;
+  }
+
+  public setClearDb(cb: (prisma: T) => Promise<void>) {
+    this.buildProps.clearDb = cb;
+    return this;
+  }
+
+  public setFillDb(cb: (prisma: T) => Promise<void>) {
+    this.buildProps.fillDb = cb;
+    return this;
+  }
+
+  public extend(builder: IntegrationTestHookBuilder<T>) {
+    this.buildProps = { ...this.buildProps, ...builder.buildProps };
+    return this;
+  }
+
+  public setCallback(cb: (moduleRef: TestingModule) => Promise<void>) {
+    this.buildProps.callback = cb;
+    return this;
+  }
+
+  public setModuleMetadata(metadata: ModuleMetadata) {
+    this.buildProps.moduleMetadata = metadata;
+    return this;
+  }
+
+  public build() {
+    return () => {
+      let prisma: T;
+
+      beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule(
+          this.buildProps.moduleMetadata
+        ).compile();
+
+        prisma = moduleRef.get<T>(this.buildProps.PrismaService);
+
+        await this.buildProps.clearDb(prisma);
+        await this.buildProps.fillDb(prisma);
+        await this.buildProps.callback(moduleRef);
+      });
+    };
+  }
+}
