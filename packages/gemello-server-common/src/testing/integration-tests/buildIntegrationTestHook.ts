caca5ff1d84309737743e5bc6e67d98afaae3576
Michał Raszczuk <michal.raszczuk.apusoft@gmail.com>
2023-10-10 21:04:21 +0200

    added override providers

--- packages/gemello-server-common/src/testing/integration-tests/buildIntegrationTestHook.ts
+++ packages/gemello-server-common/src/testing/integration-tests/buildIntegrationTestHook.ts
@@ -1,6 +1,11 @@
+import {
+  INestApplication,
+  ModuleMetadata,
+  Provider,
+  ValueProvider,
+} from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
-import { Prisma, PrismaClient } from "@prisma/client";
-import { INestApplication, ModuleMetadata } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 export interface BuildIntegrationTestHookProps<T extends PrismaClient> {
   PrismaService: { new (): T };
@@ -8,9 +13,16 @@ export interface BuildIntegrationTestHookProps<T extends PrismaClient> {
   fillDb: (prisma: T) => Promise<void>;
   callback: (moduleRef: TestingModule, app: INestApplication) => Promise<void>;
   moduleMetadata: ModuleMetadata;
+  overrideProviders: OverrideProvider[];
 }
 
 const emptyFn = async () => {};
+export type Class<T> = { new (): T };
+
+interface OverrideProvider {
+  provider: symbol | string | Class<any>;
+  value: any;
+}
 
 export class IntegrationTestHookBuilder<T extends PrismaClient> {
   private buildProps: BuildIntegrationTestHookProps<T> = {
@@ -19,6 +31,7 @@ export class IntegrationTestHookBuilder<T extends PrismaClient> {
     fillDb: emptyFn,
     clearDb: emptyFn,
     moduleMetadata: {},
+    overrideProviders: [],
   };
 
   public setPrismaClient(Prisma: { new (): T }) {
@@ -41,7 +54,9 @@ export class IntegrationTestHookBuilder<T extends PrismaClient> {
     return this;
   }
 
-  public setCallback(cb: (moduleRef: TestingModule, app: INestApplication) => Promise<void>) {
+  public setCallback(
+    cb: (moduleRef: TestingModule, app: INestApplication) => Promise<void>
+  ) {
     this.buildProps.callback = cb;
     return this;
   }
@@ -51,13 +66,29 @@ export class IntegrationTestHookBuilder<T extends PrismaClient> {
     return this;
   }
 
+  public overrideProvider(overrideProvider: OverrideProvider) {
+    this.buildProps.overrideProviders.push(overrideProvider);
+    return this;
+  }
+
   public build() {
     return () => {
       let prisma: PrismaClient;
       let app: INestApplication;
 
       beforeAll(async () => {
-        const moduleRef = await Test.createTestingModule(this.buildProps.moduleMetadata).compile();
+        let testModuleBuilder = Test.createTestingModule(
+          this.buildProps.moduleMetadata
+        );
+
+        this.buildProps.overrideProviders.forEach(({ provider, value }) => {
+          testModuleBuilder = testModuleBuilder
+            .overrideProvider(provider)
+            .useValue(value);
+        });
+
+        const moduleRef = await testModuleBuilder.compile();
+
         prisma = moduleRef.get<T>(this.buildProps.PrismaService);
         app = moduleRef.createNestApplication();
 
