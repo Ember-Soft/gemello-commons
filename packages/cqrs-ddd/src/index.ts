f78a1841ab5f0f1283a34f1dafcf6a082bc6cdb5
Michał Raszczuk <michal.raszczuk.apusoft@gmail.com>
2023-10-22 19:24:37 +0200

    added cqrs library

---
+++ packages/cqrs-ddd/src/index.ts
@@ -0,0 +1,11 @@
+export { UseCaseHandler } from "./ddd/UseCaseHandler";
+export { Aggregate } from "./ddd/Aggregate";
+export { DomainEvent } from "./ddd/DomainEvent";
+export { Entity } from "./ddd/Entity";
+export { GemelloCqrsModule } from "./ddd/GemelloCqrsModule";
+export { UseCase } from "./ddd/UseCase";
+export { ValueObject } from "./ddd/ValueObject";
+export { Command } from "./cqrs/Command";
+
+export type { IMapper } from "./ddd/IMapper";
+export type { CreateEntityProps } from "./ddd/Entity";
